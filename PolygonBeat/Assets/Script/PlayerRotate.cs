9c724943c4f03843eb0cf700eb573f21dbdc8470
hans4809 <58892107+hans4809@users.noreply.github.com>
2023-05-07 19:40:17 +0900

    fix : Player 회전 수정

--- PolygonBeat/Assets/Script/PlayerRotate.cs
+++ PolygonBeat/Assets/Script/PlayerRotate.cs
@@ -6,52 +6,55 @@ using UnityEngine;
 using static Unity.VisualScripting.AnnotationUtility;
 
 public class PlayerRotate : MonoBehaviour
-{
-    Quaternion aRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+{  
+    public Vector3 rotation;
     public Vector3 parentPosition;
     public Vector3 childPosition;
-    public GameObject parentObject1;
+    public List<GameObject> parentObject;
     public GameObject childObject;
-    public GameObject parentObject2;
-    public GameObject parentObject3;
-    public GameObject parentObject4;
     [SerializeField][Range(1f, 100f)] float rotateSpeed = 50f;
+    float time = 0f;
     void SwapParent(GameObject childObject, GameObject preParentObject, GameObject nextParentObject)
     {
         nextParentObject.transform.SetParent(null);
+        nextParentObject.transform.localEulerAngles = Vector3.zero;
         childObject.transform.SetParent(nextParentObject.transform);
-        //childPosition = childObject.transform.localPosition;
         preParentObject.transform.SetParent(childObject.transform);
-        parentPosition = childObject.transform.parent.localPosition;
-        Debug.Log(parentPosition);
-        childObject.transform.parent.SetLocalPositionAndRotation(new Vector3(parentPosition.x, 0.08f, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+        //childObject.transform.parent.transform.position = parentPosition;
     }
 
     void Start()
     {
-        parentObject1.transform.SetParent(null);
-        childObject.transform.SetParent(parentObject1.transform);
-        aRotation = childObject.transform.parent.transform.rotation;
+        parentObject[0].transform.SetParent(null);
+        childObject.transform.SetParent(parentObject[0].transform);
+        //parentPosition = childObject.transform.parent.transform.position;
+        rotation = childObject.transform.parent.transform.localEulerAngles;
     }
     void Update()
     {
-        if ( (int)Time.time % 4 == 1 && childObject.transform.parent == parentObject1.transform)
+        rotation = Vector3.Lerp(new Vector3(0,0,0), new Vector3(0,0,-90), time);
+        childObject.transform.parent.transform.localEulerAngles = rotation;
+        time += Time.deltaTime * rotateSpeed;
+        if (time > 1f)
         {
-            SwapParent(childObject, parentObject1, parentObject2);
+            time = 0f;
+            if (childObject.transform.parent == parentObject[0].transform)
+            {
+                SwapParent(childObject, parentObject[0], parentObject[1]);
+            }
+            else if (childObject.transform.parent == parentObject[1].transform)
+            {
+                SwapParent(childObject, parentObject[1], parentObject[2]);
+            }
+            else if (childObject.transform.parent == parentObject[2].transform)
+            {
+                SwapParent(childObject, parentObject[2], parentObject[3]);
+            }
+            else if (childObject.transform.parent == parentObject[3].transform)
+            {
+                SwapParent(childObject, parentObject[3], parentObject[0]);
+            }
         }
-        else if ((int)Time.time % 4 == 2 && childObject.transform.parent == parentObject2.transform)
-        {
-            SwapParent(childObject, parentObject2, parentObject3);
-        }
-        else if ((int)Time.time % 4 == 3 && childObject.transform.parent == parentObject3.transform)
-        {
-            SwapParent(childObject, parentObject3, parentObject4);
-        }
-        else if ((int)Time.time % 4 == 0 && childObject.transform.parent == parentObject4.transform)
-        {
-            SwapParent(childObject, parentObject4, parentObject1);
-        }
-        aRotation = aRotation * Quaternion.Euler(new Vector3(0, 0, -90) * Time.deltaTime * rotateSpeed);
-        childObject.transform.parent.transform.rotation = aRotation;
+            
     }
 }
