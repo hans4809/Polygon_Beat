c6e34a79f2c752f09fed9aa4dad3c66f717460da
hans4809 <58892107+hans4809@users.noreply.github.com>
2023-05-08 15:56:27 +0900

    fix : Player 회전이 될 수록 조금씩 올라가는 거 수정

--- PolygonBeat/Assets/Script/PlayerRotate.cs
+++ PolygonBeat/Assets/Script/PlayerRotate.cs
@@ -8,51 +8,59 @@ using static Unity.VisualScripting.AnnotationUtility;
 public class PlayerRotate : MonoBehaviour
 {  
     public Vector3 rotation;
-    public Vector3 parentPosition;
-    public Vector3 childPosition;
     public List<GameObject> parentObject;
+    public Vector3 parentPosition;
     public GameObject childObject;
     [SerializeField][Range(1f, 100f)] float rotateSpeed = 50f;
     float time = 0f;
     void SwapParent(GameObject childObject, GameObject preParentObject, GameObject nextParentObject)
     {
+        parentPosition.x += 1;
         nextParentObject.transform.SetParent(null);
         nextParentObject.transform.localEulerAngles = Vector3.zero;
         childObject.transform.SetParent(nextParentObject.transform);
         preParentObject.transform.SetParent(childObject.transform);
-        //childObject.transform.parent.transform.position = parentPosition;
+        childObject.transform.parent.SetLocalPositionAndRotation(new Vector3(parentPosition.x, 0.11f, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
     }
-
+    void SetRotation(GameObject childObject)
+    {
+        for(int i=0; i < 3; i++)
+            childObject.transform.GetChild(i).localEulerAngles = Vector3.zero;
+    }
     void Start()
     {
         parentObject[0].transform.SetParent(null);
         childObject.transform.SetParent(parentObject[0].transform);
-        //parentPosition = childObject.transform.parent.transform.position;
         rotation = childObject.transform.parent.transform.localEulerAngles;
+        parentPosition = childObject.transform.parent.localPosition;
     }
     void Update()
     {
         rotation = Vector3.Lerp(new Vector3(0,0,0), new Vector3(0,0,-90), time);
         childObject.transform.parent.transform.localEulerAngles = rotation;
         time += Time.deltaTime * rotateSpeed;
-        if (time > 1f)
+        if (time > rotateSpeed)
         {
             time = 0f;
             if (childObject.transform.parent == parentObject[0].transform)
             {
                 SwapParent(childObject, parentObject[0], parentObject[1]);
+                SetRotation(childObject);
             }
             else if (childObject.transform.parent == parentObject[1].transform)
             {
                 SwapParent(childObject, parentObject[1], parentObject[2]);
+                SetRotation(childObject);
             }
             else if (childObject.transform.parent == parentObject[2].transform)
             {
                 SwapParent(childObject, parentObject[2], parentObject[3]);
+                SetRotation(childObject);
             }
             else if (childObject.transform.parent == parentObject[3].transform)
             {
                 SwapParent(childObject, parentObject[3], parentObject[0]);
+                SetRotation(childObject);
             }
         }
             
