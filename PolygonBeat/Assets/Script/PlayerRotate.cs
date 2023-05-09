70f3a3ec9ad24d627a5f60c5e1a5286f231755ce
hans4809 <58892107+hans4809@users.noreply.github.com>
2023-05-09 23:18:10 +0900

    fix : Rotate 속도 조정

--- PolygonBeat/Assets/Script/PlayerRotate.cs
+++ PolygonBeat/Assets/Script/PlayerRotate.cs
@@ -11,7 +11,8 @@ public class PlayerRotate : MonoBehaviour
     public List<GameObject> parentObject;
     public Vector3 parentPosition;
     public GameObject childObject;
-    [SerializeField][Range(1f, 100f)] float rotateSpeed = 50f;
+    public AnalyzeExample analyzeExample;
+    float rotateSpeed;
     float time = 0f;
     void SwapParent(GameObject childObject, GameObject preParentObject, GameObject nextParentObject)
     {
@@ -20,8 +21,8 @@ public class PlayerRotate : MonoBehaviour
         nextParentObject.transform.localEulerAngles = Vector3.zero;
         childObject.transform.SetParent(nextParentObject.transform);
         preParentObject.transform.SetParent(childObject.transform);
-        childObject.transform.parent.SetLocalPositionAndRotation(new Vector3(parentPosition.x, 0.11f, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
-        childObject.transform.SetLocalPositionAndRotation(new Vector3(-5, 5, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+        childObject.transform.parent.SetLocalPositionAndRotation(new Vector3(parentPosition.x, 0.11f, 0), Quaternion.Euler(new Vector3(0, 0, 0))); // parent 오차 수정
+        childObject.transform.SetLocalPositionAndRotation(new Vector3(-5, 5, 0), Quaternion.Euler(new Vector3(0, 0, 0))); // child 오차 수정
     }
     void SetRotation(GameObject childObject)
     {
@@ -30,19 +31,26 @@ public class PlayerRotate : MonoBehaviour
     }
     void Start()
     {
+        int i = 0;
         parentObject[0].transform.SetParent(null);
+        parentPosition = parentObject[0].transform.localPosition;
         childObject.transform.SetParent(parentObject[0].transform);
-        rotation = childObject.transform.parent.transform.localEulerAngles;
-        parentPosition = childObject.transform.parent.localPosition;
+        childObject.transform.parent.localEulerAngles = Vector3.zero;
+        childObject.transform.localEulerAngles = Vector3.zero;
+        childObject.transform.parent.localPosition = parentPosition;
+        childObject.transform.localPosition = new Vector3(-5, 5, 0);
+        rotateSpeed = 1 / (analyzeExample.beats[i+1].timestamp - analyzeExample.beats[i].timestamp);
     }
     void Update()
     {
+        int i = 1;
         rotation = Vector3.Lerp(new Vector3(0,0,0), new Vector3(0,0,-90), time);
         childObject.transform.parent.transform.localEulerAngles = rotation;
-        time += Time.deltaTime * rotateSpeed;
-        if (time / rotateSpeed >= 1)
+        time += Time.deltaTime * rotateSpeed; 
+        if (time >= 1)
         {
             time = 0f;
+            rotateSpeed = 1 / (analyzeExample.beats[i+1].timestamp - analyzeExample.beats[i].timestamp); // rotate 속도 각 비트의 시간 차이에 따라 달라짐
             if (childObject.transform.parent == parentObject[0].transform)
             {
                 SwapParent(childObject, parentObject[0], parentObject[1]);
@@ -63,6 +71,9 @@ public class PlayerRotate : MonoBehaviour
                 SwapParent(childObject, parentObject[3], parentObject[0]);
                 SetRotation(childObject);
             }
+            i++;
+            if (i > analyzeExample.beats.Count)
+                rotateSpeed = 0;
         }
             
     }
