9fd56ee0eb0d78e0673b4f81a007cec1117e6812
hororok <jeongeunpark777@gmail.com>
2023-05-25 01:14:31 +0900

    클릭시 clear 클릭 안할시 miss뜨게

--- PolygonBeat/Assets/Script/TouchCheck.cs
+++ PolygonBeat/Assets/Script/TouchCheck.cs
@@ -7,82 +7,81 @@ using Unity.VisualScripting;
 
 public class TouchCheck : MonoBehaviour
 {
+    float clickedtime;
+    bool clicked;
+    bool cleared;
     Queue<float> Boundary = new Queue<float>();
-    Queue<float> CorrectArea = new Queue<float>();
     float BeforeBoundary;
     public AnalyzeExample analyzeExample;
     int i;
-    float range;
     float time;
-      
+
 
     private void Start()
     {
         time = 0.0f;
-        BeforeBoundary= 0.0f;
-        i = 0;
+        BeforeBoundary = 0.0f;
+        clicked = false;
+        cleared = false;
         BeatTimeInform();
         
+
+        
+
     }
 
     private void BeatTimeInform() //비트 받아오기
     {
-        
 
+        
         for (i = 0; i < analyzeExample.beats.Count - 1; i++)
         {
             Boundary.Enqueue((analyzeExample.beats[i].timestamp + analyzeExample.beats[i + 1].timestamp) / 2); //경계값 설정
         }
 
-
-        for (i = 0; i < analyzeExample.beats.Count - 1; i++)
-        {
-            CorrectArea.Enqueue((analyzeExample.beats[i].timestamp + analyzeExample.beats[i + 1].timestamp) *9 / 20); //오차허용범위
-        }
+     
     }
 
-    
-    
+
+
     private void Update()
     {
         BeatTimeInform();
 
         time += Time.deltaTime;
 
-        while (time >= BeforeBoundary && time < Boundary.Peek()) 
+        if(Input.GetMouseButtonDown(0)) 
         {
-            if (Input.GetMouseButtonDown(0))
+            clickedtime = time;
+            if (clickedtime >= BeforeBoundary && time < Boundary.Peek()) 
             {
-                if (time < analyzeExample.beats[i].timestamp - CorrectArea.Peek())
-                {
-                    Debug.Log("Miss"); //-하트
-                }
-
-                else if (time >= analyzeExample.beats[i].timestamp - CorrectArea.Peek() && time <= analyzeExample.beats[i].timestamp + CorrectArea.Peek())
+                if (!clicked) 
                 {
                     Debug.Log("Clear");
+                    cleared = true;
+                    clicked = true;
                 }
-
-                else if (time > analyzeExample.beats[i].timestamp + CorrectArea.Peek())
-                {
-                    Debug.Log("Miss"); //-하트
-                }
-
-                BeforeBoundary = Boundary.Dequeue();
-                CorrectArea.Dequeue();
             }
-
-            else 
-            {
-                Debug.Log("Miss"); //-하트
-                BeforeBoundary = Boundary.Dequeue();
-                CorrectArea.Dequeue();
-            }
-
-
         }
-        
-        
+
+        if (time >= Boundary.Peek()) 
+        {
+            if (!clicked && !cleared) 
+            {
+                Debug.Log("Miss");
+                
+            }
+
+            cleared = false;
+            clicked = false;
+            BeforeBoundary = Boundary.Dequeue();
+            Boundary.Dequeue();
+        }
+
+
+
+
+       
+
     }
 }
-
