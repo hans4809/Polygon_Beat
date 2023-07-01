cc39e56c9289d80a831dee6b6a6b5fabf5a9b4d8
hans4809 <58892107+hans4809@users.noreply.github.com>
2023-07-01 21:23:01 +0900

    feat : 게임 오버 시 씬 변경대신 캔버스로 활용함

--- PolygonBeat/Assets/Script/Manager/LifeManager.cs
+++ PolygonBeat/Assets/Script/Manager/LifeManager.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class LifeManager : MonoBehaviour
 {
     [SerializeField] private int life = 3;
     [SerializeField] List<Sprite> lifeSprite;
-    [SerializeField] List<Image> lifeImage;
+    [SerializeField] List<UnityEngine.UI.Image> lifeImage;
+    [SerializeField] List<UnityEngine.UI.Button> buttonList;
+    [SerializeField] GameObject gameOverCanvas;
+    [SerializeField] MusicPlayerManager musicPlayerManager;
 
     // Start is called before the first frame update
     void Start()
     {
+        musicPlayerManager = FindAnyObjectByType<MusicPlayerManager>();
         life = 3;
         for(int i = 0; i < life; i++)
         {
             lifeImage[i].sprite = lifeSprite[0];
         }
+        buttonList[0].onClick.AddListener(ChangetoSelectScene);
+        buttonList[1].onClick.AddListener(Replay);
     }
     public void Init()
     {
@@ -48,9 +56,19 @@ public class LifeManager : MonoBehaviour
 
     public void GameOver()
     {
-        SceneManager.LoadScene("GameOver");
-    }
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+        musicPlayerManager.StopBGM(DataManager.singleTon.wholeGameData._currentSong.ToString());
 
+    }
+    public void ChangetoSelectScene()
+    {
+        SceneManager.LoadScene("StageSelect");
+    }
+    public void Replay()
+    {
+
+    }
     private void Update()
     {
         if (life <= 0)
--- PolygonBeat/Assets/Script/Manager/SceneChanger.cs
+++ PolygonBeat/Assets/Script/Manager/SceneChanger.cs
@@ -7,21 +7,15 @@ using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    public List<Button> buttonList;
+
     // Start is called before the first frame update
     void Start()
     {
-        buttonList[0].onClick.AddListener(ChangetoSelectScene);
-        buttonList[1].onClick.AddListener(ChangetoplayScene);
+
+
     }
-    public void ChangetoplayScene()
-    {
-        SceneManager.LoadScene("GameScene");
-    }
-    public void ChangetoSelectScene()
-    {
-        SceneManager.LoadScene("StageSelect");
-    }
+
+
     // Update is called once per frame
     void Update()
     {
